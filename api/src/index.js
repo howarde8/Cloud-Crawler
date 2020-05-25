bf899bdcb920db83d1ab62c121a4d381d23791c4
Howard <howarde8@gmail.com>
2020-05-25 23:33:36 +0800

    Add Apollo server

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,22 @@
-console.log('Hello Cloud Crawler');
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    description: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    description: () => 'This is a cloud crawler system',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Apollo server is running at ${url}`);
+});
