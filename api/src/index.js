ddd0b32b563b464f20651c213d33d0de9a67a5d6
Howard <howarde8@gmail.com>
2020-07-19 18:26:32 +0800

    Update POST /api/crawl with database

--- api/src/index.js
+++ api/src/index.js
@@ -4,18 +4,37 @@ const bodyParser = require('body-parser');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const pubsub = require('./pubsub');
-const db = require('./db');
-
-const tempCrawls = require('./fakeDb');
+const { Crawl } = require('./db');
 
 const app = express();
 app.use(bodyParser());
-app.post('/api/crawl', (req, res) => {
+app.post('/api/crawl', async (req, res) => {
+  const { id, status, body } = req.body;
+  if (!id) {
+    return res.status(400).send('ID is missing');
+  }
+  if (!status) {
+    return res.status(400).send('Status not found');
+  } else if (status !== 'FAILED' && status !== 'SUCCESS') {
+    return res.status(400).send('Status invalid, should be FAILED or SUCCESS');
+  }
+  if (body) {
+    try {
+      JSON.parse(body);
+    } catch (err) {
+      return res.status(400).send('Body invalud, must be JSON array format');
+    }
+  }
+
+  const data = await Crawl.update({ status, result: body }, { where: { id } });
+  if (data[0] !== 1) {
+    return res.status(500).send('ID not found');
+  }
+  const crawl = await Crawl.findByPk(id);
+  pubsub.publish('CRAWL_UPDATED', { crawlUpdated: crawl });
   res.sendStatus(200);
-  console.log(req.body);
-  tempCrawls[req.body.id] = { ...tempCrawls[req.body.id], ...req.body };
-  pubsub.publish('CRAWL_UPDATED', { crawlUpdated: req.body });
 });
+
 app.listen(5000, () => console.log('Express server is listening at port 5000'));
 
 const server = new ApolloServer({
